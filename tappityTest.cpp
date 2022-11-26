4b03d725d7ec40b0bf60ea4b4594193cbcc5c594
Kevin Buffardi <kbuffardi@csuchico.edu>
2022-11-25 20:24:43 -0800

    Add skeleton

---
+++ tappityTest.cpp
@@ -0,0 +1,17 @@
+/**
+ * Unit Test suite
+**/
+
+#include <gtest/gtest.h>
+#include "tappity.h"
+ 
+class tappityTest : public ::testing::Test
+{
+	protected:
+		tappityTest(){} 
+		virtual ~tappityTest(){} 
+		virtual void SetUp(){}
+		virtual void TearDown(){}
+};
+
+
